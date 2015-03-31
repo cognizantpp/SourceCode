6d6d034b6b81431cbf7a8a51991dc65e6a4338db
cognizantpp <marchfastnow@gmail.com>
2015-03-31 17:29:35 +0530

    number entry view controller.m file is added

---
+++ WoundCare/NumberEntryViewController.m
@@ -0,0 +1,73 @@
+//
+//  NumberEntryViewController.m
+//  WoundCare
+//
+//  Created by Sangeetha on 24/03/15.
+//
+//
+
+#import "NumberEntryViewController.h"
+#import "AddAssignmentPopOverController.h"
+#import "CoreDataHelper.h"
+@interface NumberEntryViewController ()
+
+@end
+
+@implementation NumberEntryViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.entryData = [[NSMutableString alloc]init];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)numberClicked:(UIButton*)sender {
+    
+    switch (sender.tag) {
+        case 11:
+            [_entryData appendString:@"."];
+            break;
+        case 12:
+            [_entryData deleteCharactersInRange:  NSMakeRange([_entryData length]-1, 1)];
+            break;
+        default:
+            [_entryData appendString:[NSString stringWithFormat:@"%ld",(long)sender.tag]];
+            break;
+    }
+    NSLog(@"length %lu",(unsigned long)[_entryData length]);
+    if([_entryData length] >6){
+        UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Alert" message:@"Entry number should be only 6 digits" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alertView show];
+        [_entryData deleteCharactersInRange:  NSMakeRange([_entryData length]-1, 1)];
+    }
+    else{
+        [self.delegate updateEntryNumber:_entryData];
+    }
+}
+- (IBAction)btnGoClicked:(id)sender {
+    CoreDataHelper *coreDataHelper = [CoreDataHelper sharedInstance];
+    BOOL valid = YES;//[coreDataHelper validateEntryNumber:_entryData];
+    if (valid) {
+        [self.delegate OkClicked];
+    }
+    else{
+        UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"Alert" message:@"Entry number should be unique" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alertView show];
+    }
+    [self.delegate OkClicked];
+}
+@end
